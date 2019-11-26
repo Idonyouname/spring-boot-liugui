b08f2ae0c44a92877392ecc93c3817c3c28f02a1
Idonyouname <13539506534@163.com>
2019-11-26 18:02:03 +0800

    log 11/26

---
+++ spring-boot-log/src/test/java/com/amdox/liugui/springbootlog/SpringBootLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amdox.liugui.springbootlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
