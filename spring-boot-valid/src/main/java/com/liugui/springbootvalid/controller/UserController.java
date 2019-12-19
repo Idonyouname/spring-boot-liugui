c708e85e6bca1da4b01dd3cfed28f8eae2fda81e
Idonyouname <13539506534@163.com>
2019-12-19 15:14:45 +0800

    valid 12/19

--- spring-boot-valid/src/main/java/com/liugui/springbootvalid/controller/UserController.java
+++ spring-boot-valid/src/main/java/com/liugui/springbootvalid/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 /**
